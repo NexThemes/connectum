c166344556793e5c710f57236d681783a0b9998e
Valentin <vscarevnea@gmail.com>
2018-11-12 22:05:48 +0200

    Test 1

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import classes from "./Layout.css";
+
+class Layout extends Component {
+    render() {
+        return (
+            <div className={classes.Layout}>Layout</div>
+        )
+    }
+}
+
+export default Layout;
